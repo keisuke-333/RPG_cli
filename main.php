3dd6b946c5aec4013e8eb01b10cad6f5e2dbaf1d
keisuke-333 <keikubo.999@gmail.com>
2021-03-25 21:10:03 +0900

    main.php編集

--- main.php
+++ main.php
@@ -2,22 +2,52 @@
 require_once('./classes/Human.php');
 require_once('./classes/Enemy.php');
 require_once('./classes/Brave.php');
+require_once('./classes/BlackMage.php');
+require_once('./classes/WhiteMage.php');
 
-$tiida = new Brave("ティーダ");
-$goblin = new Enemy("ゴブリン");
+$members = array();
+$members[] = new Brave('ティーダ');
+$members[] = new WhiteMage('ユウナ');
+$members[] = new BlackMage('ルールー');
+
+$enemies = array();
+$enemies[] = new Enemy('ゴブリン', 20);
+$enemies[] = new Enemy('ボム', 25);
+$enemies[] = new Enemy('モルボル', 30);
 
 $turn = 1;
 
 while ($tiida->getHitPoint() > 0 && $goblin->getHitPoint() > 0) {
   echo "*** $turn ターン目 ***\n\n";
 
-  echo $tiida->getName() . " : " . $tiida->getHitPoint() . "/" . $tiida::MAX_HITPOINT . "\n";
-  echo $goblin->getName() . " : " . $goblin->getHitPoint() . "/" . $goblin::MAX_HITPOINT . "\n";
+  foreach ($members as $member) {
+    echo $member->getName() . " ： " . $member->getHitPoint() . "/" . $member::MAX_HITPOINT . "\n";
+  }
+  echo "\n";
+  foreach ($enemies as $enemy) {
+    echo $enemy->getName() . " ： " . $enemy->getHitPoint() . "/" . $enemy::MAX_HITPOINT . "\n";
+  }
   echo "\n";
 
-  $tiida->doAttack($goblin);
+  foreach ($members as $member) {
+    $enemyIndex = rand(0, count($enemies) - 1); // 添字は0から始まるので、-1する
+    $enemy = $enemies[$enemyIndex];
+    // 白魔道士の場合、味方のオブジェクトも渡す
+    if (get_class($member) == "WhiteMage") {
+        $member->doAttackWhiteMage($enemy, $member);
+    } else {
+        $member->doAttack($enemy);
+    }
   echo "\n";
-  $goblin->doAttack($tiida);
+  }
+  echo "\n";
+
+  foreach ($enemies as $enemy) {
+    $memberIndex = rand(0, count($members) - 1); // 添字は0から始まるので、-1する
+    $member = $members[$memberIndex];
+    $enemy->doAttack($member);
+    echo "\n";
+  }
   echo "\n";
 
   $turn ++;
